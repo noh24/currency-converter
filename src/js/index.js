1bda8ea460f932f01342f9e2f81f098172c81565
Brian Noh <noh24@ymail.com>
2022-11-18 14:20:39 -0500

    add function to handle submission and print result

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,24 @@
 import ExchangeRateService from './../services/exchange-rate-service.js';
+import { getExchangeRate } from './exchange-rate.js';
 
 window.addEventListener('load', function() {
   addSelectBoxes();
+  document.querySelector('form').addEventListener('submit', handleSubmission);
 });
+
+export function printError(response) {
+  document.getElementById('error').innerText = `There was an error with ExchangeRate-API: ${response}`;
+}
+export function printResults(convertedAmount, baseCurrency, amount, exchangeCurrency) {
+  document.getElementById('results').innerText = `${amount} ${baseCurrency} = ${convertedAmount} ${exchangeCurrency}`;
+}
+function handleSubmission(e) {
+  e.preventDefault();
+  const baseCurrency = document.getElementById('base-currency').value;
+  const amount = document.getElementById('amount').value;
+  const exchangeCurrency = document.getElementById('exchange-currency').value;
+  getExchangeRate(baseCurrency, amount, exchangeCurrency);
+}
 
 async function addSelectBoxes() {
   let response = await ExchangeRateService.getExchangeRate('USD');
