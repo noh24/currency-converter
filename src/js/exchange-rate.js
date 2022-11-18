c4fac07f4434e3b01b4a94c93f7e6697879c1b9d
Brian Noh <noh24@ymail.com>
2022-11-18 15:37:53 -0500

    wip: add conditional to fetch

--- src/js/exchange-rate.js
+++ src/js/exchange-rate.js
@@ -2,15 +2,22 @@ import ExchangeRateService from "./../services/exchange-rate-service";
 import { printError, printResults, printRates } from "./index.js";
 
 export async function getExchangeRate(baseCurrency, amount, exchangeCurrency) {
-  const response = await ExchangeRateService.getExchangeRate(baseCurrency);
-  if (response instanceof Error) {
-    printError(response);
+  if (!sessionStorage || Object.keys(sessionStorage).includes(baseCurrency)) {
+    console.log(`working`);
+    const response = await ExchangeRateService.getExchangeRate(baseCurrency);
+    if (response instanceof Error) {
+      printError(response);
+    } else {
+      storeSession(response);
+      calculateExchange(baseCurrency, amount, exchangeCurrency);
+      printRates(baseCurrency);
+    }
   } else {
-    storeSession(response);
+    console.log(`not printing a new one but new money`);
     calculateExchange(baseCurrency, amount, exchangeCurrency);
-    printRates(baseCurrency);
   }
 }
+
 
 function storeSession(response) {
   for (const [key, value] of Object.entries(response['conversion_rates'])) {
