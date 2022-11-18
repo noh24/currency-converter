3266e3e19f471c74b817ad94d62730e967415959
Brian Noh <noh24@ymail.com>
2022-11-18 14:44:03 -0500

    add print rates function

--- src/js/index.js
+++ src/js/index.js
@@ -12,8 +12,32 @@ export function printError(response) {
 export function printResults(convertedAmount, baseCurrency, amount, exchangeCurrency) {
   document.getElementById('results').innerText = `${amount} ${baseCurrency} = ${convertedAmount} ${exchangeCurrency}`;
 }
+export function printRates(baseCurrency) {
+  const conversionTable = document.getElementById('conversion-table');
+  const table = document.createElement('table');
+  const tableHeader = document.createElement('th');
+  const tableHeader2 = document.createElement('th');
+  const tableHeader3 = document.createElement('th');
+  tableHeader.innerText = 'Base Currency';
+  tableHeader2.innerText = 'Exchange Currency';
+  tableHeader3.innerText = 'Exchange Rate';
+  table.append(tableHeader, tableHeader2, tableHeader3);
+  for (const [key, value] of Object.keys(sessionStorage)) {
+    const tableData = document.createElement('td');
+    const tableData2 = document.createElement('td');
+    const tableData3 = document.createElement('td');
+    tableData.innerText = baseCurrency;
+    tableData2.innerText = key;
+    tableData3.innerText = value;
+    const tableRow = document.createElement('tr');
+    tableRow.append(tableData, tableData2, tableData3);
+    table.append(tableRow);
+  }
+  conversionTable.append(table);
+}
 function handleSubmission(e) {
   e.preventDefault();
+  document.getElementById('conversion-table').innerText = null;
   const baseCurrency = document.getElementById('base-currency').value;
   const amount = document.getElementById('amount').value;
   const exchangeCurrency = document.getElementById('exchange-currency').value;
