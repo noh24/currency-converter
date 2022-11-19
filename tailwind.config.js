973eb1c5915f40c4b75d7bdee5ba5b876f97e5eb
Brian Noh <noh24@ymail.com>
2022-11-18 23:57:07 -0500

    add safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html, js}'],
+  content: ['./src/**/*.{html,js}'],
+  safelist: [
+    'font-normal',
+    'text-5xl',
+    'p-5',
+    'py-3',
+    'px-6',
+    'text-blue-500'
+  ],
   theme: {
     extend: {
       fontFamily: {
